822811204729d7f6b07082ef2b9040b2f340acce
Kaleb <kcky7d@umsystem.edu>
2021-04-19 20:45:23 -0400

    Sortable Table complete

--- user_stories.py
+++ user_stories.py
@@ -1,8 +1,51 @@
-from flask import Flask, render_template, request, url_for, flash
+from flask import Flask, render_template, request, url_for, flash, Markup
 import string
 from db.model import tools_db, user_stories, work_roles
+from flask_table import Table, Col, ButtonCol
 
 app = Flask(__name__)
+
+
+class SortableTable(Table):
+    id = Col('ID')
+    tool = Col('Tool')
+    work_role = Col('Work Role')
+    user_story = Col('User Story', allow_sort=False)
+    vote = ButtonCol('Vote', 'upvote', text_fallback='+',allow_sort=False)
+    allow_sort = True
+
+    def sort_url(self, col_key, reverse=False):
+        if reverse:
+            direction = 'desc'
+        else:
+            direction = 'asc'
+
+        return url_for('previous_submissions', sort=col_key, direction=direction)
+
+class Item(object):
+    def __init__(self, id, tool, work_role, user_story):
+        self.id = id
+        self.tool = tool
+        self.work_role = work_role
+        self.user_story = user_story
+
+    @classmethod
+    def get_elements(cls):
+        element_list = []
+
+        for story in user_stories:
+            element_list.append(Item(story['id'], story['tool'],
+                                story['work_role'], story['user_story']))
+        return element_list
+
+    @classmethod
+    def get_sorted_by(cls, sort, reverse=False):
+        return sorted(
+            cls.get_elements(),
+            key=lambda x: getattr(x, sort),
+            reverse=reverse)
+            
+
 
 def strip_punctuation(submission):
     print(submission['feature'].translate(str.maketrans('','',string.punctuation)))
@@ -47,10 +90,20 @@ def successful_submission():
 
 @app.route("/previous_submissions")
 def previous_submissions():
-    return render_template("previous_submissions.html", user_stories=user_stories)
+    sort = request.args.get('sort', 'id')
+    reverse = (request.args.get('direction', 'asc') == 'desc')
+    table = SortableTable(Item.get_sorted_by(sort, reverse),
+                                                sort_by=sort,
+                                                sort_reverse=reverse)
+    table_html = Markup(table.__html__())
+    print(table_html)
+    return render_template("previous_submissions.html", table_html=table_html)
+
+    # return table.__html__()
 
 
-@app.route("/upvote")
+
+@app.route("/upvote", methods=["GET", "POST"])
 def upvote():
     return render_template("upvote.html")
 
